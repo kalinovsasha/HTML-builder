0765d8b4faec62d15d95b7742b18e36b895fd9e5
Alexander <kalinovsasha@yandex.ru>
2022-05-17 21:45:11 +0300

    feat: add solution 02-write-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,2 +1,49 @@
 const fs = require('fs');
 const path = require('path');
+const readline = require('node:readline');
+const process = require('process');
+
+const filePath = path.join(__dirname,'file.txt');
+const rl = readline.createInterface({ input:process.stdin, output:process.stdout});
+const stream = fs.createWriteStream (filePath);
+
+rl.on('SIGINT', () => {
+    console.log(`Файл ${filePath} создан!`);
+    rl.close();
+  });
+rl.on('line', (line) => {
+    if (line === 'exit' || line === 'Exit') {
+        console.log(`Файл ${filePath} создан!`);
+        rl.close();
+    } else {
+        stream.write(`${line}\n`);
+    }
+  });
+
+
+
+
+
+/*//Проверка на существование файла
+fs.access(filePath, fs.F_OK, (err) => {
+    if (err) {
+      console.error("err")
+      return
+    }
+    //file exists
+  })
+*/
+//fs.createReadStream(path.join(__dirname, './file.txt'), 'utf8').pipe(process.stdout);
+
+
+
+
+
+
+
+
+
+
+
+
+
