28a0aee267032b25a1e97de6422b3d4fb79cf7af
Alexander <kalinovsasha@yandex.ru>
2022-05-26 17:46:40 +0300

    refactor: change function name

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -8,13 +8,11 @@ class BundleCollector {
   }
   collect(){
     const stream = fs.createWriteStream (this.bundlePath);
-    let buff=[];
     fs.readdir(this.stylesPath, {withFileTypes: true}, (error, files) => {
       files.forEach((file)=>{
         if(file.isFile() && path.extname(path.join(this.stylesPath, file.name))==='.css'){
           fs.readFile(path.join(path.join(this.stylesPath, file.name)), 'utf-8', (err, data) => {
-            buff.push(data);
-            stream.write(buff.join('\n'));
+            stream.write(data+'\n');
           });
         }         
       });
@@ -25,15 +23,7 @@ class BundleCollector {
 const stylesPath = path.join(__dirname,'styles');
 const bundlePath = path.join(__dirname,'project-dist','bundle.css');
 
-let a= new BundleCollector(stylesPath,bundlePath);
-a.collect();
-/*
-fs.readdir(stylesPath, {withFileTypes: true}, (error, files) => {
-    files.forEach((file)=>{
-        if(file.isFile() && path.extname(path.join(stylesPath, file.name))==='.css'){
-            console.log(file.name);
-        };      
-    });
-});
-*/
+let bundleCollector= new BundleCollector(stylesPath,bundlePath);
+bundleCollector.collect();
+
 
